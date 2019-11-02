8f7d189fdf7e78d90b70fbfaedfe453413c557d5
Javier Vega Irribarra <uo233544@uniovi.es>
2019-11-02 01:04:18 +0100

    Cambiando detalles

--- tew1-301-306-311/src/com/tew/presentation/BeanCitas.java
+++ tew1-301-306-311/src/com/tew/presentation/BeanCitas.java
@@ -3,7 +3,7 @@ package com.tew.presentation;
 import java.io.Serializable;
 import java.util.ResourceBundle;
 
-import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.*;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
@@ -11,26 +11,26 @@ import com.tew.business.CitasService;
 import com.tew.infrastructure.Factories;
 import com.tew.model.Cita;
 
+/**
+ * Se añade este atributo de entidad para recibir la cita en concreto
+ * selecionada de la tabla o de un formulario. Es necesario inicializarlo para
+ * que al entrar desde el formulario de altaCitaForm.xml se puedan dejar los
+ * avalores en un objeto existente.
+ * 
+ */
+@ManagedBean(name = "control")
+@SessionScoped
 public class BeanCitas implements Serializable {
-
-	/**
-	 * Se añade este atributo de entidad para recibir la cita en concreto
-	 * selecionada de la tabla o de un formulario. Es necesario inicializarlo para
-	 * que al entrar desde el formulario de altaCitaForm.xml se puedan dejar los
-	 * avalores en un objeto existente.
-	 * 
-	 */
 	private static final long serialVersionUID = 7944042841591604009L;
 
 	private Cita[] citas = null;
 
 	// uso de inteccción de dependencia
-	@ManagedProperty(value = "#{alumno}")
+	@ManagedProperty(value = "#{cita}")
 	private BeanCita cita;
 
 	public BeanCita getCita() {
 		return cita;
-
 	}
 
 	public void setCta(BeanCita cita) {
@@ -39,6 +39,15 @@ public class BeanCitas implements Serializable {
 
 	public Cita[] getCitas() {
 		return (citas);
+	}
+
+	public void inicaCitas(ActionEvent event) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		// Obtenemos el archivo de propiedades correspondiente al idioma que
+		// tengamos seleccionado y que viene envuelto en facesContext
+		@SuppressWarnings("unused")
+		ResourceBundle bundle = facesContext.getApplication().getResourceBundle(facesContext, "msgs");
+		cita.setIdPiso(0);
 	}
 
 	public String listado() {
@@ -52,15 +61,6 @@ public class BeanCitas implements Serializable {
 			e.printStackTrace();
 			return "error";
 		}
-	}
-	
-	public void inicaCita(ActionEvent event) {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		// Obtenemos el archivo de propiedades correspondiente al idioma que
-		// tengamos seleccionado y que viene envuelto en facesContext
-		@SuppressWarnings("unused")
-		ResourceBundle bundle = facesContext.getApplication().getResourceBundle(facesContext, "msgs");
-		cita.setIdPiso(0);
 	}
 
 }
